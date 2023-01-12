7c0a042c179ab1c909d0572f0a758f274e334ddb
dxkrnn <dxkrn@gmail.com>
2023-01-13 00:00:50 +0700

    Add: hide pass on SignupPage

--- lib/pages/login_registration/signup_with_email_page.dart
+++ lib/pages/login_registration/signup_with_email_page.dart
@@ -2,15 +2,26 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:neo_nft/controllers/signup_controller.dart';
 import 'package:neo_nft/theme.dart';
 import 'package:neo_nft/widgets/custom_appbar.dart';
 import 'package:neo_nft/widgets/custom_button.dart';
 import 'package:neo_nft/widgets/custom_button_border_with_icon.dart';
+import 'package:neo_nft/widgets/custom_password_text_field.dart';
 import 'package:neo_nft/widgets/custom_text_field.dart';
 import 'package:neo_nft/widgets/login_signup_background.dart';
 
-class SignupWithEmailPage extends StatelessWidget {
+class SignupWithEmailPage extends StatefulWidget {
   const SignupWithEmailPage({super.key});
+
+  @override
+  State<SignupWithEmailPage> createState() => _SignupWithEmailPageState();
+}
+
+class _SignupWithEmailPageState extends State<SignupWithEmailPage> {
+  //NOTE: Checkbox Controller
+  final SignupCheckBoxController signupCheckBoxController =
+      Get.put(SignupCheckBoxController());
 
   @override
   Widget build(BuildContext context) {
@@ -60,69 +71,93 @@ class SignupWithEmailPage extends StatelessWidget {
                               title: 'Email address',
                               hint: 'Enter your email address...',
                             ),
-                            CustomTextField(
+                            CustomPasswordTextField(
                               title: 'Password',
                               hint: 'Enter your password...',
-                              hidePass: true,
-                              withIcon: true,
                             ),
-                            CustomTextField(
+                            CustomConfirmPasswordTextField(
                               title: 'Confirm Password',
                               hint: 'Enter your password...',
-                              hidePass: true,
-                              withIcon: true,
                             ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  width: 18.w,
-                                  height: 18.w,
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      width: 1,
-                                      color: whiteColor.withOpacity(0.7),
-                                    ),
+                            Obx(
+                              () => Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  signupCheckBoxController.isChecked.value
+                                      ? GestureDetector(
+                                          child: SizedBox(
+                                            width: 18.w,
+                                            height: 18.w,
+                                            child: const Image(
+                                              image: AssetImage(
+                                                  'assets/icons/icon_check.png'),
+                                            ),
+                                          ),
+                                          onTap: () {
+                                            signupCheckBoxController
+                                                    .isChecked.value =
+                                                !signupCheckBoxController
+                                                    .isChecked.value;
+                                          })
+                                      : GestureDetector(
+                                          child: Container(
+                                            width: 18.w,
+                                            height: 18.w,
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                width: 1,
+                                                color:
+                                                    whiteColor.withOpacity(0.7),
+                                              ),
+                                            ),
+                                          ),
+                                          onTap: () {
+                                            signupCheckBoxController
+                                                    .isChecked.value =
+                                                !signupCheckBoxController
+                                                    .isChecked.value;
+                                          }),
+                                  SizedBox(
+                                    width: 10.w,
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 10.w,
-                                ),
-                                SizedBox(
-                                  width: deviceWidth - 82.w,
-                                  child: RichText(
-                                    text: TextSpan(
-                                      text: 'I Agree to the ',
-                                      style: regularTextStyle.copyWith(
-                                        fontSize: 14.sp,
-                                        color: whiteColor.withOpacity(0.7),
+                                  SizedBox(
+                                    width: deviceWidth - 82.w,
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: 'I Agree to the ',
+                                        style: regularTextStyle.copyWith(
+                                          fontSize: 14.sp,
+                                          color: whiteColor.withOpacity(0.7),
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text: 'Terms & Conditions',
+                                            style: boldTextStyle.copyWith(
+                                              fontSize: 14.sp,
+                                              decoration:
+                                                  TextDecoration.underline,
+                                            ),
+                                            recognizer: TapGestureRecognizer()
+                                              ..onTap = () {
+                                                print(
+                                                    'Terms & Condition Tapped');
+                                              },
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                ' of using NEO NFT Market Mobile App.',
+                                            style: regularTextStyle.copyWith(
+                                              fontSize: 14.sp,
+                                              color:
+                                                  whiteColor.withOpacity(0.7),
+                                            ),
+                                          ),
+                                        ],
                                       ),
-                                      children: [
-                                        TextSpan(
-                                          text: 'Terms & Conditions',
-                                          style: boldTextStyle.copyWith(
-                                            fontSize: 14.sp,
-                                            decoration:
-                                                TextDecoration.underline,
-                                          ),
-                                          recognizer: TapGestureRecognizer()
-                                            ..onTap = () {
-                                              print('Terms & Condition Tapped');
-                                            },
-                                        ),
-                                        TextSpan(
-                                          text:
-                                              ' of using NEO NFT Market Mobile App.',
-                                          style: regularTextStyle.copyWith(
-                                            fontSize: 14.sp,
-                                            color: whiteColor.withOpacity(0.7),
-                                          ),
-                                        ),
-                                      ],
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                             SizedBox(
                               height: 60.h,
